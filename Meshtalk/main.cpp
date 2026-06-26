61c7d15e112a46aa97102792a891e61f51579945
Aashutosh Dahal <aashutosh6725@student.ku.edu.np>
2026-06-26 09:32:18 +0545

    Update CMakeLists and main.cpp

--- Meshtalk/main.cpp
+++ Meshtalk/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("Meshtalk", "Main");
+    engine.loadFromModule("MeshTalk", "Main");
 
     return QGuiApplication::exec();
 }
